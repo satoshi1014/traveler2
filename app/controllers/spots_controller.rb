43091bef3bf2e2382a12dec3bd36e8ed7567b931
satoshi1014 <f.satoshi.1014@gmail.com>
2021-02-09 10:16:55 +0900

    newの作成と編集、画像はまだやっていない

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -2,4 +2,18 @@ class SpotsController < ApplicationController
   def index
     @spots = Spot.all
   end
+
+  def new
+    @spot = Spot.new
+  end
+
+  def create
+    @spot = Spot.create(spot_params)
+    redirect_to root_path
+  end 
+
+  private
+  def spot_params
+    params.require(:spot).permit(:place, :address, :one_word, :thought)
+  end
 end
