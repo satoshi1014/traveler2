9a5087fa38cbe1804ab6a98ac04e9e13ed7f1a08
satoshi1014 <f.satoshi.1014@gmail.com>
2021-02-09 09:47:47 +0900

    migration_fileを編集

--- db/migrate/20210209004309_create_spots.rb
+++ db/migrate/20210209004309_create_spots.rb
@@ -1,7 +1,11 @@
 class CreateSpots < ActiveRecord::Migration[6.1]
   def change
     create_table :spots do |t|
-
+      t.string  :place
+      t.string  :address
+      t.string  :one_word
+      t.text  :thought
+      t.integer :user_id
       t.timestamps
     end
   end
