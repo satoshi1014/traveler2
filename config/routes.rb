dfd8ea78d6e3a6512df1159dd8363c9d1f629b3b
satoshi1014 <f.satoshi.1014@gmail.com>
2021-02-13 18:51:57 +0900

    ルーティング編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'spots#index'
   resources :spots
-  resources :users only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update]
 end
