624cc44c89282891452ad9f8f135b53d699c9310
satoshi1014 <f.satoshi.1014@gmail.com>
2021-02-14 15:24:14 +0900

    アソシエーションをやった

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many  :spots
+  has_many  :spots, dependent: :destroy
+  has_many :comments
+
 end
