052efc5e93b11a5cf5cac91fc6508c62f7f4e721
satoshi1014 <f.satoshi.1014@gmail.com>
2021-02-14 15:42:51 +0900

    headerにマイページボタンを設置

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_13_041424) do
+ActiveRecord::Schema.define(version: 2021_02_14_062101) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -40,6 +40,16 @@ ActiveRecord::Schema.define(version: 2021_02_13_041424) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "comment_content"
+    t.integer "user_id", null: false
+    t.integer "spot_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["spot_id"], name: "index_comments_on_spot_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "spots", force: :cascade do |t|
     t.string "place"
     t.string "address"
@@ -67,4 +77,6 @@ ActiveRecord::Schema.define(version: 2021_02_13_041424) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "spots"
+  add_foreign_key "comments", "users"
 end
